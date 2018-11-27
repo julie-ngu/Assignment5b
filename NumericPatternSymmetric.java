a69484693036ba343a97cedfe2a31db380b81dfd
julie-ngu <31775733+julie-ngu@users.noreply.github.com>
2018-11-26 22:55:39 -0500

    Update NumericPatternSymmetric.java

--- NumericPatternSymmetric.java
+++ NumericPatternSymmetric.java
@@ -26,28 +26,35 @@ public class NumericPatternSymmetric {
 	
 	public static String symmetricPattern(int num) {
 		// method that produces a symmetric pattern about n where each half before and after n is also symmetric around n-1
+		
+		// make a variable to hold the text for where the whole pattern is contained (output)
 		String output = "";
+		// make a variable to hold the text for where the repetitions (1 2 1) is contained (nextOutput)
 		String nextOuput;
 		
 		if(num >= 15 || num < 1) {
-			// error
+			// returns error if number is greater than/equal to 15 or number is less than 1
 			output = "Error";
 			return output;
 		}
 		else if(num == 1) {
-			// base case (n = 1)
+			// base case (n = 1), prints out " 1 " if number is 1
 			output = " 1 ";
 			return output;
 		}
 		else {
 			// for numbers greater than one but less or equal to 15
 			nextOuput = symmetricPattern(num - 1);
+			// assign recursion to variable nextOutput so you don't need to type in that same line of code over and over
 			if(num <= 5) {
+				//if number is less than or equal to 5, let the output be assigned nextOuput (recursion) + number + nextOutput(recursion)
 				output = nextOuput + num + nextOuput;
 			}
 			else {
+				//if the number is greater than 5, do the same thing from if statement, except add a new line before the next recursion
 				output = nextOuput + num + "\n" + nextOuput;
 			}
+			//return output after if-else
 			return output;
 		}
 	}
